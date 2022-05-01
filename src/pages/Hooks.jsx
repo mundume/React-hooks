bb2cc5f881bead5250379abe8204c624cd377453
Nzai <nzai@localhost.localdomain>
2022-05-01 14:23:56 +0300

    useReduce

--- src/pages/Hooks.jsx
+++ src/pages/Hooks.jsx
@@ -1,16 +1,29 @@
 import React from 'react'
-import { useState } from'react'
+import { useReducer} from'react'
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'increment':
+      return {count: state.count + 1, showText: state.showText}
+   case "toggleShowText":
+     return {count: state.count, showText:!state.showText}
+
+     default:
+      return state;
+  }
+}
 
 const Hooks = () => {
-    const [value, setInputValue] = useState('')
-    let onChange = (e) => {
-        let  nzz = (e.target.value)
-        setInputValue (nzz)
-    } 
+    const [state, dispatch] = useReducer(reducer, {count: 0,showText:true})
   return (
     <div>
-        <input placeholder='enter text' onChange={onChange}/>
-        {value}
+      <h1>{state.count}</h1>
+        <button onClick={()=>{
+          dispatch({type: 'increment'})
+          dispatch ({type: 'toggleShowText'})
+        }}> click</button>
+
+        {state.showText&&<h1>Hello</h1>}
     </div>
   )
 }
